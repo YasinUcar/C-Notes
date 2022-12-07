c1a8aec4a75d50c266a7d5899267bfe7506fe325
Yasin Uçar <yasinucar@protonmail.com>
2022-12-08 01:46:36 +0300

    abs interface and singleton

---
+++ 02MyFiles/AbstractandInterface/Program.cs
@@ -0,0 +1,65 @@
+﻿#region Interface : Sözleşme () bir şeyin yapılıp yapılamaması (CanAbleTo)
+//Interface'lerin içerisine kod yazamayız bunlar sözleşmeyi sağlar abstract class ile farkı budur
+
+
+public interface IVehicle
+{
+    void Go();
+    void Stop();
+}
+public interface IRideable
+{
+    void Ride();
+}
+public interface IFlyable
+{
+    void Soar();
+}
+class Car : IVehicle
+{
+    public void Go()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+class Bisiklet : IVehicle, IRideable
+{
+    public void Ride()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IVehicle.Go()
+    {
+        throw new NotImplementedException();
+    }
+
+    void IVehicle.Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+#endregion
+
+#region : Abstract (Soyutlama) Class 
+public class SqlServerDb
+{
+    public string GetDbVersion() => "Sql Server 2012";
+    public string GenererateSql(int id)
+    {
+        return $"SELECT + FROM USERS WHERE ID = {id}";
+    }
+    public void ExecuteSql(string sql)
+    {
+        //generateSql
+    }
+}
+
+#endregion
+
+
---
+++ 02MyFiles/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿Singleton singleton = Singleton.GetInstance();
+
+
+
+
+public class Singleton
+{
+    private static Singleton instance = null;
+    private string text;
+    private Singleton()
+    {
+        text = "hello world!";
+    }
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+    public string GetText()
+    {
+        return text;
+    }
+}
+
