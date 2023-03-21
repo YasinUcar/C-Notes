5599565c7dccd4e59e85488eb0fe5bd691200334
Yasin Ucar <yasinucar@protonmail.com>
2023-03-21 22:06:12 +0300

    open closed principle

---
+++ 02MyFiles/Design Principles/OpenClosedPrinciple.cs
@@ -0,0 +1,55 @@
+﻿class ParaGonderici
+{
+
+}
+interface IBanka
+{
+    bool ParaTransferleri(int tutar, string hesapNo);
+}
+class Garanti : IBanka
+{
+    public string HesapNo { get; set; }
+    public void ParaGonder(int tutar)
+    {
+
+    }
+
+    public bool ParaTransferleri(int tutar, string hesapNo)
+    {
+        try
+        {
+            HesapNo = hesapNo;
+            ParaGonder(tutar);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+class HalkBank : IBanka
+{
+    string _hesapNo;
+    public void GonderilecekHesapNo(string hesapNo)
+    {
+
+    }
+    public void Gonder(int tutar)
+    {
+
+    }
+    public bool ParaTransferleri(int tutar, string hesapNo)
+    {
+        try
+        {
+            GonderilecekHesapNo(hesapNo);
+            Gonder(tutar);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
