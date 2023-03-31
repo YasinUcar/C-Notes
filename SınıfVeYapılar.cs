b0652122f3f5ad5f1a1ae55a57db3447ae2767af
Yasin Ucar <yasinucar@protonmail.com>
2023-04-01 01:10:06 +0300

    SınıfveYapılar

---
+++ SınıfVeYapılar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SampleCodes.ClassandBuilding
+{
+    public class SınıfVeYapılar : MonoBehaviour
+    {
+
+        #region Base Class : Bu sınıftan bir nesne üretilmeyecek ancak kalıtım yoluyla başka sınıflardan türeyecek
+        abstract class Chacters
+        {
+            public string g_ChacterName;
+            public int g_ChacterLevel;
+            public float g_ChacterHealth;
+        }
+        #endregion
+
+        #region Interface :  Projede olabildiğince aktif olarak kullnılacak, kod karmaşasını önleyecek şekilde kullanılmalıdır.
+        interface IPlayer { }
+        interface IEnemy { }
+        interface IRigidbody { }
+        interface INavmesh { }
+        interface INPC { }
+        #endregion
+
+        #region Class : Tek sorumluluğu olması gerekir PlayerClass'ı içerisinde hem Movement hem Attack hem Diyalog aynı aynda olmamalıdır.
+        public class Movement { }
+        public class Attack { }
+        public class Dialog { }
+
+        #endregion
+
+    }
+}
